74e9e8fd2d090fa0ec2093e876464a79a4bc1f73
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-02-21 11:35:08 +0200

    Cleaned up Login, Making ContactPage.

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,12 @@
+let contactEmailInput;
+let contactPasswordInput;
+let contactMessageInput;
+let btnSubmitContactForm;
+
+
+const initElems = ()=> {
+    contactEmailInput = document.getElementById("contact-input-email");
+    contactPasswordInput = document.getElementById("contact-input-password");
+    contactMessageInput = document.getElementById("contact-input-message");
+    btnSubmitContactForm = document.getElementById("contact-btn");
+}
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,73 +4,85 @@ import validateEmail from "../validation/validateEmail.js";
 import validatePassword from "../validation/validatePassword.js";
 import { initializeProfile } from "./ProfilePage.js"; 
 
-const loginEmailInput = document.getElementById("login-input-email");
-const loginPasswordInput = document.getElementById("login-input-password");
-const loginRegisterIfNoAccountLink = document.getElementById("login-goto-register-page");
-const btnLogin = document.getElementById("login-btn");
+let loginEmailInput;
+let loginPasswordInput;
+let loginRegisterIfNoAccountLink;
+let btnLogin;
 
 window.addEventListener("load", () => {
-  //when page loaded
+    //when page is loaded
+    initElems();
+    initEventListeners();
     loginEmailInput.value = "";
     loginPasswordInput.value = "";
 });
 
-loginEmailInput.addEventListener("input", () => {
-    let errorArr = validateEmail(loginEmailInput.value);
-    if (errorArr.length === 0) {
-        //no error
-        loginEmailInput.classList.remove("is-invalid");
-        document.getElementById("login-alert-email").classList.add("d-none");
-    } else {
-        // error/s
-        loginEmailInput.classList.add("is-invalid");
-        document.getElementById("login-alert-email").classList.remove("d-none");
-        document.getElementById("login-alert-email").innerHTML = errorArr.join("<br>"); 
-    }
+const initElems = ()=> {
+    loginEmailInput = document.getElementById("login-input-email");
+    loginPasswordInput = document.getElementById("login-input-password");
+    loginRegisterIfNoAccountLink = document.getElementById("login-goto-register-page");
+    btnLogin = document.getElementById("login-btn");
+}
+
+const initEventListeners = () => {
+    loginEmailInput.addEventListener("input", () => {
+        let errorArr = validateEmail(loginEmailInput.value);
+        if (errorArr.length === 0) {
+            //no error
+            loginEmailInput.classList.remove("is-invalid");
+            document.getElementById("login-alert-email").classList.add("d-none");
+        } else {
+            // error/s
+            loginEmailInput.classList.add("is-invalid");
+            document.getElementById("login-alert-email").classList.remove("d-none");
+            document.getElementById("login-alert-email").innerHTML = errorArr.join("<br>"); 
+        }
 });
 
-loginPasswordInput.addEventListener("input", () => {
-    let errorArr = validatePassword(loginPasswordInput.value);
-    if (errorArr.length === 0) {
-        //no error
-        loginPasswordInput.classList.remove("is-invalid");
-        document.getElementById("login-alert-password").classList.add("d-none");
-    } else {
-        // error/s
-        loginPasswordInput.classList.add("is-invalid");
-        document.getElementById("login-alert-password").classList.remove("d-none");
-        document.getElementById("login-alert-password").innerHTML =
-        errorArr.join("<br>");
-    }
-});
+    loginPasswordInput.addEventListener("input", () => {
+        let errorArr = validatePassword(loginPasswordInput.value);
+        if (errorArr.length === 0) {
+            //no error
+            loginPasswordInput.classList.remove("is-invalid");
+            document.getElementById("login-alert-password").classList.add("d-none");
+        } else {
+            // error/s
+            loginPasswordInput.classList.add("is-invalid");
+            document.getElementById("login-alert-password").classList.remove("d-none");
+            document.getElementById("login-alert-password").innerHTML =
+            errorArr.join("<br>");
+        }
+    });
 
-btnLogin.addEventListener("click", () => {
-    if (validateEmail(loginEmailInput.value).length) {
-        return;
-    }
-    if (validatePassword(loginPasswordInput.value).length) {
-        return;
-    }
-    let users = JSON.parse(localStorage.getItem("users"));
-    if (!users) {
-        //users === null
-        return;
-    }
-    let user = users.find(
-        (currUser) =>
-        currUser.email === loginEmailInput.value &&
-        currUser.password === loginPasswordInput.value);
-        
-    if (!user) {
-        console.log("invalid email and/or password");
-        return;
-    }
-    localStorage.setItem("token", JSON.stringify({id: user.id, first_name:user.first_name, 
-        last_name: user.last_name, email:user.email, isAdmin: user.isAdmin}));
-    initializeProfile(user);
-    location.reload();
-});
+    btnLogin.addEventListener("click", () => {
+        if (validateEmail(loginEmailInput.value).length) {
+            return;
+        }
+        if (validatePassword(loginPasswordInput.value).length) {
+            return;
+        }
+        let users = JSON.parse(localStorage.getItem("users"));
+        if (!users) {
+            //users === null
+            return;
+        }
+        let user = users.find(
+            (currUser) =>
+            currUser.email === loginEmailInput.value &&
+            currUser.password === loginPasswordInput.value);
+            
+        if (!user) {
+            console.log("invalid email and/or password");
+            return;
+        }
+        localStorage.setItem("token", JSON.stringify({id: user.id, first_name:user.first_name, 
+            last_name: user.last_name, email:user.email, isAdmin: user.isAdmin}));
+        initializeProfile(user);
+        location.reload();
+    });
 
-loginRegisterIfNoAccountLink.addEventListener("click", ()=>{
-    handlePageChange(PAGES.REGISTER);
-})
+    loginRegisterIfNoAccountLink.addEventListener("click", ()=>{
+        handlePageChange(PAGES.REGISTER);
+    })
+}
+
