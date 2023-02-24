49dcdac3921e29668bac7e6700db1717f500137c
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-02-24 13:05:20 +0200

    Cleaned up Carousel for good.

--- src/components/PicturesCarousel.js
+++ src/components/PicturesCarousel.js
@@ -1,8 +1,5 @@
 let picturesArr;
 let carouselDiv;
-let showIdx;
-let animationStarted;
-let picIds;
 let carouselNextBtn;
 
 const initializePicturesCarousel = (picturesArrFromHomePage) => {
@@ -12,44 +9,18 @@ const initializePicturesCarousel = (picturesArrFromHomePage) => {
 }
 
 const updatePicturesCarousel = (picturesArrFromHomePage) => {
-    picturesArr = picturesArrFromHomePage;
-    picIds = picturesArr.map(picture => picture.id);
-    showIdx = 0;   
+    picturesArr = picturesArrFromHomePage; 
     createCarousel();
 }
 
-// const createCarouselItem = (id, src, alt, credit) => {
-//     return `
-//     <div id="carousel-item-${id}" class="opacity-0 d-none carousel-picture">
-//         <img src="${src}"
-//             alt="${alt}" class="carousel-center-img" />
-//         <span class="d-flex justify-content-center">Photographed by: ${credit}</span>
-//     </div>
-//     `;
-// };
-
 const createCarouselItem = (id, src, alt, credit) => {
-    return `
-    <div class="carousel-item">
-        <div class="card carousel-picture border-0" 
-        id="carousel-item-${id}">
-            <img src="${src}" class="card-img-top img-fluid carousel-center-img" alt="${alt}">
-            <div class="card-body">
-                <p class="card-text text-center">Photographed by: ${credit}</p>
+    return `<div class="carousel-item" id="carousel-item-${id}">
+                <img src="${src}" class="d-block carousel-center-img" alt="${alt}">
+                <span class="d-flex justify-content-center">Photographed by: ${credit}
+                </span>
             </div>
-        </div>
-    </div>
-    `;
-};
-
-// const createCarouselItem = (id, src, alt, credit) => {
-//     return `<div class="carousel-item">
-//                 <img src="${src}" class="d-block carousel-center-img" alt="${alt}">
-//                 <span class="d-flex justify-content-center">Photographed by: ${credit}
-//                 </span>
-//             </div>
-//     `
-// }
+    `
+}
 
 const createCarousel = () => {
     let buffer = "";
@@ -58,7 +29,6 @@ const createCarousel = () => {
     }
     carouselDiv.innerHTML = buffer;
     document.querySelector(`.carousel-item`).classList.add("active");
-    //document.querySelector(`.carousel-picture`).classList.remove("border-0");
 }
 
 const initBtns = () => {
