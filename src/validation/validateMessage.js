9bf16ec99b2c8f64afc879d7ebde5d1f23747ede
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-02-27 15:38:37 +0200

    Deleted Unused File

--- src/validation/validateMessage.js
+++
@@ -1,7 +0,0 @@
-import validate from "./validate.js";
-const validateMessage = (value) => {
-    const reg = new RegExp("^[a-zA-Z ]*$", "ig");
-    return validate(reg, value, 10, 20).map((err) => `Messsageis ${err}`);
-};
-
-export default validateString;
